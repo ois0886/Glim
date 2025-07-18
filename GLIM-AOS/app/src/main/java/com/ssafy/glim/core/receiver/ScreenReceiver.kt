b38e769af3c2bde7fe89a0b0cec7dd09a9a26d7e
park99999 <sungung1026@naver.com>
2025-07-18 16:14:17 +0900

    feat : lockScrreen

---
+++ GLIM-AOS/app/src/main/java/com/ssafy/glim/core/receiver/ScreenReceiver.kt
@@ -0,0 +1,26 @@
+package com.ssafy.glim.core.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.ssafy.glim.feature.lock.LockScreenActivity
+
+object ScreenReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        when(intent.action) {
+            Intent.ACTION_SCREEN_ON -> {
+                navigateToLock(context)
+            }
+        }
+    }
+
+    private fun navigateToLock(context: Context) {
+        context.startActivity(
+            Intent(context, LockScreenActivity::class.java).apply {
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+                addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
+            }
+        )
+    }
+}
