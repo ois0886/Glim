88125a0676feff03cf1e8b7f8e4fa0783dd4a25d
yunjunseok <ujs9901@naver.com>
2025-07-24 23:12:11 +0900

    JWT 토큰 하드코딩된 값들 상수화

---
+++ GEULGWI-BACKEND/src/main/java/com/lovedbug/geulgwi/config/SecurityConstants.java
@@ -0,0 +1,8 @@
+package com.lovedbug.geulgwi.config;
+
+public class SecurityConstants {
+    private SecurityConstants(){}
+
+    public static final String HEADER_AUTH = "Authorization";
+    public static final String TOKEN_PREFIX = "Bearer ";
+}
