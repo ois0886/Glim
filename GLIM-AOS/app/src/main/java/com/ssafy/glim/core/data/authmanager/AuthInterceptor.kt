79e96d0bd86ad6ec28ad19531237d6754c7e2cbe
ootr47 <ootr45@gmail.com>
2025-07-26 00:18:39 +0900

    feat: 토큰 더미데이터 제거

--- GLIM-AOS/app/src/main/java/com/ssafy/glim/core/data/authmanager/AuthInterceptor.kt
+++ GLIM-AOS/app/src/main/java/com/ssafy/glim/core/data/authmanager/AuthInterceptor.kt
@@ -9,7 +9,7 @@ class AuthInterceptor @Inject constructor(
     private val authManager: AuthManager
 ) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        val token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJob25nQG5hdmVyLmNvbSIsImlhdCI6MTc1MzQ1MDI2NiwiZXhwIjoxNzUzNDUzODY2LCJtZW1iZXJJZCI6MiwidHlwZSI6ImFjY2VzcyIsImp0aSI6IjE0NmJlMjRhLTg0YWYtNGJlMy1hMTM3LTBhMTQ0MWRmMTM4OSJ9.4VPp77DIi-FCHsKJwClppofW54pXgxB1pNOTg6v8SBE"
+        val token = authManager.getAccessToken()
 
         if (token == null) {
             Log.d("AuthInterceptor", "Token is null. Authorization header not added.")
