3e3df4333a6910d0dba53710c8be57ffe9a81411
yunjunseok <ujs9901@naver.com>
2025-07-25 14:13:30 +0900

    상수화 시킨 클래스 제거

--- GEULGWI-BACKEND/src/main/java/com/lovedbug/geulgwi/config/SecurityConstants.java
+++
@@ -1,8 +0,0 @@
-package com.lovedbug.geulgwi.config;
-
-public class SecurityConstants {
-    private SecurityConstants(){}
-
-    public static final String HEADER_AUTH = "Authorization";
-    public static final String TOKEN_PREFIX = "Bearer ";
-}
