185aa47642385876e43463e18280ba44a2bf1391
devMuscle <hongsbien@naver.com>
2025-07-28 15:52:41 +0900

    공통 최상위 exception 정의

--- GEULGWI-BACKEND/src/main/java/com/lovedbug/geulgwi/core/domain/member/exception/EmailAlreadyVerifiedException.java
+++ GEULGWI-BACKEND/src/main/java/com/lovedbug/geulgwi/core/domain/member/exception/EmailAlreadyVerifiedException.java
@@ -1,6 +1,8 @@
 package com.lovedbug.geulgwi.core.domain.member.exception;
 
-public class EmailAlreadyVerifiedException extends RuntimeException {
+import com.lovedbug.geulgwi.core.common.exception.GeulgwiException;
+
+public class EmailAlreadyVerifiedException extends GeulgwiException {
 
     public EmailAlreadyVerifiedException(String message){
         super(message);
