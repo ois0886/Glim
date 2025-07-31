c873f3031d98f6b59201b9a16c019e0b5ea029f2
devMuscle <hongsbien@naver.com>
2025-07-31 16:34:53 +0900

    id로 글귀 조회하는 api 구현

--- GEULGWI-BACKEND/src/main/java/com/lovedbug/geulgwi/core/domain/quote/QuoteRepository.java
+++ GEULGWI-BACKEND/src/main/java/com/lovedbug/geulgwi/core/domain/quote/QuoteRepository.java
@@ -1,6 +1,7 @@
 package com.lovedbug.geulgwi.core.domain.quote;
 
 import java.util.List;
+import java.util.Optional;
 import com.lovedbug.geulgwi.core.domain.quote.entity.Quote;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -25,4 +26,6 @@ public interface QuoteRepository extends JpaRepository<Quote, Long> {
     @Query("SELECT q FROM Quote q JOIN FETCH q.book WHERE q.visibility = 'PUBLIC' ORDER BY function('random')")
     List<Quote> findPublicQuotesByRandom(Pageable pageable);
 
+    @EntityGraph(attributePaths = {"book", "likes"})
+    Optional<Quote> findByQuoteIdAndVisibility(Long quoteId, String visibility);
 }
