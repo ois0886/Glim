d8352a333074221baaa25e0886b57b23669d756f
devMuscle <hongsbien@naver.com>
2025-08-04 14:01:46 +0900

    redis, embedded redis 관련 설정 추가

---
+++ GEULGWI-BACKEND/src/main/java/com/lovedbug/geulgwi/core/common/config/EmbeddedRedisConfig.java
@@ -0,0 +1,27 @@
+package com.lovedbug.geulgwi.core.common.config;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import redis.embedded.RedisServer;
+
+@Configuration
+@Profile({"local", "test"})
+public class EmbeddedRedisConfig {
+
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void startRedis() {
+        redisServer = new RedisServer(6379);
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        if (redisServer != null) {
+            redisServer.stop();
+        }
+    }
+}
