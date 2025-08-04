3a6ffcd1e16c3ab653033f81dcd78c1ba86bbd4e
park99999 <sungung1026@naver.com>
2025-08-04 14:00:02 +0900

    Feat : Admin 메인 큐레이션 생성 API

--- GEULGWI-BACKEND/src/main/java/com/lovedbug/geulgwi/core/domain/admin/AdminService.java
+++ GEULGWI-BACKEND/src/main/java/com/lovedbug/geulgwi/core/domain/admin/AdminService.java
@@ -1,8 +1,19 @@
 package com.lovedbug.geulgwi.core.domain.admin;
 
 
+import com.lovedbug.geulgwi.core.domain.admin.dto.request.CreateCurationRequest;
+import com.lovedbug.geulgwi.core.domain.admin.dto.response.CreateCurationResponse;
+import com.lovedbug.geulgwi.core.domain.admin.exception.CurationListEmptyException;
+import com.lovedbug.geulgwi.core.domain.curation.constant.CurationType;
 import com.lovedbug.geulgwi.core.domain.curation.dto.response.CurationItemResponse;
+import com.lovedbug.geulgwi.core.domain.curation.entity.CurationItem;
+import com.lovedbug.geulgwi.core.domain.curation.entity.CurationItemBook;
+import com.lovedbug.geulgwi.core.domain.curation.entity.CurationItemQuote;
+import com.lovedbug.geulgwi.core.domain.curation.entity.MainCuration;
 import com.lovedbug.geulgwi.core.domain.curation.mapper.CurationMapper;
+import com.lovedbug.geulgwi.core.domain.curation.repository.CurationItemBookRepository;
+import com.lovedbug.geulgwi.core.domain.curation.repository.CurationItemQuoteRepository;
+import com.lovedbug.geulgwi.core.domain.curation.repository.CurationItemRepository;
 import com.lovedbug.geulgwi.core.domain.curation.repository.MainCurationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,6 +25,9 @@ import java.util.stream.Stream;
 public class AdminService {
 
     private final MainCurationRepository mainCurationRepository;
+    private final CurationItemRepository curationItemRepository;
+    private final CurationItemBookRepository curationItemBookRepository;
+    private final CurationItemQuoteRepository curationItemQuoteRepository;
 
     public List<CurationItemResponse> getMainCurationByAdmin() {
         final long MAIN_CURATION_ID = 1L;
@@ -35,5 +49,43 @@ public class AdminService {
         return CurationMapper.toCurationItemDtoListFromQuotes(
             mainCurationRepository.findCurationQuotesByCurationId(curationId)
         );
+    }
+
+    public CreateCurationResponse createCuration(CreateCurationRequest createCurationRequest) {
+        MainCuration mainCuration = mainCurationRepository.save(
+                MainCuration.builder().build()
+        );
+        CurationItem item = CurationItem.builder()
+                .mainCurationId(mainCuration.getMainCurationId())
+                .title(createCurationRequest.getName())
+                .description(createCurationRequest.getDescription())
+                .curationType(createCurationRequest.getCurationType())
+                .build();
+        curationItemRepository.save(item);
+        if(createCurationRequest.getIds() != null){
+            createCurationRequest.getIds().forEach(id -> {
+                 if(createCurationRequest.getCurationType() == CurationType.BOOK){
+                     CurationItemBook curationItemBook = CurationItemBook.builder()
+                             .curationItemId(item.getCurationItemId())
+                             .bookId(id)
+                             .build();
+                     curationItemBookRepository.save(curationItemBook);
+                 }
+                 else{
+                     CurationItemQuote curationItemQuote = CurationItemQuote.builder()
+                             .curationItemId(item.getCurationItemId())
+                             .quoteId(id)
+                             .build();
+                     curationItemQuoteRepository.save(curationItemQuote);
+                 }
+            });
+        }
+        else{
+            throw new CurationListEmptyException("curation id list가 empty입니다.");
+        }
+
+        return CreateCurationResponse.builder()
+                .mainCurationId(mainCuration.getMainCurationId())
+                .build();
     }
 }
