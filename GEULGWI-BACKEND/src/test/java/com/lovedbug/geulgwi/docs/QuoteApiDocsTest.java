f63448f6bb71e1d8600b3d3c00e511832906160d
yunjunseok <ujs9901@naver.com>
2025-08-06 17:57:56 +0900

    필요없는 의존성 삭제

--- GEULGWI-BACKEND/src/test/java/com/lovedbug/geulgwi/docs/QuoteApiDocsTest.java
+++ GEULGWI-BACKEND/src/test/java/com/lovedbug/geulgwi/docs/QuoteApiDocsTest.java
@@ -26,7 +26,6 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
