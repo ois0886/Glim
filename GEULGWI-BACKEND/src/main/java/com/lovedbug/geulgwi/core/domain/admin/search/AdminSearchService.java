2a46ccdebf62b6a0c00a71e366279479b3d0a5db
park99999 <sungung1026@naver.com>
2025-08-06 14:15:06 +0900

    feat : admin member api(전부 조회, active 된 특정 사용자 조회, active 모든 사용자 조회, 수정, 삭제, 검색)

--- GEULGWI-BACKEND/src/main/java/com/lovedbug/geulgwi/core/domain/admin/search/AdminSearchService.java
+++ GEULGWI-BACKEND/src/main/java/com/lovedbug/geulgwi/core/domain/admin/search/AdminSearchService.java
@@ -6,12 +6,17 @@ import com.lovedbug.geulgwi.core.domain.admin.mapper.SearchBookMapper;
 import com.lovedbug.geulgwi.core.domain.admin.mapper.SearchQuoteMapper;
 import com.lovedbug.geulgwi.core.domain.book.BookRepository;
 import com.lovedbug.geulgwi.core.domain.book.entity.Book;
+import com.lovedbug.geulgwi.core.domain.member.Member;
+import com.lovedbug.geulgwi.core.domain.member.MemberRepository;
+import com.lovedbug.geulgwi.core.domain.member.dto.response.MemberResponse;
+import com.lovedbug.geulgwi.core.domain.member.mapper.MemberMapper;
 import com.lovedbug.geulgwi.core.domain.quote.repository.QuoteRepository;
 import com.lovedbug.geulgwi.core.domain.quote.entity.Quote;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +24,7 @@ public class AdminSearchService {
 
     private final BookRepository bookRepository;
     private final QuoteRepository quoteRepository;
+    private final MemberRepository memberRepository;
 
     public List<SearchBookResponse> searchBooks(String keyword, Pageable page) {
         List<Book> books = bookRepository.findByTitleContainingIgnoreCase(keyword, page).getContent();
@@ -28,5 +34,12 @@ public class AdminSearchService {
     public List<SearchQuoteResponse> searchQuotes(String keyword, Pageable page) {
         List<Quote> quotes = quoteRepository.findByContentContainingIgnoreCase(keyword, page).getContent();
         return SearchQuoteMapper.toDtoList(quotes);
+    }
+
+    public List<MemberResponse> searchMembers(String keyword, Pageable page) {
+        List<Member> members = memberRepository.searchByKeyword(keyword, page).getContent();
+        return members.stream()
+            .map(MemberMapper::toMemberDto)
+            .collect(Collectors.toList());
     }
 }
