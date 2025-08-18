25167176b6355f5cb2675acb779c8b078ff118ba
박승준 <83055885+ootr47@users.noreply.github.com>
2025-08-18 11:32:11 +0900

    refactor: 중복 제거

--- app/src/main/java/com/example/myapplication/core/navigation/internal/navigator/InternalNavigator.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myapplication.core.navigation.internal.navigator
-
-import kotlinx.coroutines.channels.Channel
-
-internal interface InternalNavigator {
-    val channel: Channel<InternalRoute>
-}
